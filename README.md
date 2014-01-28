88b10138be81fb4a126550d579c60e3fd3dc8b89
barbushin <barbushin@gmail.com>
2014-01-28 19:29:28 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-php-console-laravel
-===================
+## Laravel service provider for PHP Console
 
-Service provider for integration PHP Console library with Laravel framework
+[![Latest Stable Version](https://poser.pugx.org/php-console/laravel-service-provider/version.png)](https://packagist.org/packages/php-console/laravel-service-provider)
+
+PHP Console allows you to handle PHP errors & exceptions, dump variables, execute PHP code remotely and many other things using [Google Chrome extension PHP Console](https://chrome.google.com/webstore/detail/php-console/nfhmhhlpfleoednkpnnnkolmclajemef) and [PhpConsole server library](https://github.com/barbushin/php-console).
+
+This packages integrates [PHP Console server library](https://github.com/barbushin/php-console) with [Laravel framework](http://laravel.com) as configurable service provider.
+
+## Installation
+
+Require this package in Laravel project `composer.json` and run `composer update`:
+
+    "php-console/laravel-service-provider": "1.*"
+
+After updating composer, add the ServiceProvider to the providers array in app/config/app.php
+
+    'PhpConsole\Laravel\ServiceProvider',
+
+You can also publish the service provider config-file and feel free to edit it.
+
+    php artisan config:publish php-console/laravel-service-provider
---
+++ src/PhpConsole/Laravel/ServiceProvider.php
@@ -0,0 +1,150 @@
+<?php namespace PhpConsole\Laravel;
+
+use Config;
+use PhpConsole\Handler;
+use PhpConsole\Connector;
+use PhpConsole\Helper;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider {
+
+	const PACKAGE_ALIAS = 'php-console';
+
+	/** @var bool Is PHP Console server enabled */
+	protected $isEnabled = true;
+	/** @var string Path to PhpConsole classes directory */
+	protected $phpConsolePathAlias = 'application.vendors.PhpConsole.src.PhpConsole';
+	/** @var string Base path of all project sources to strip in errors source paths */
+	protected $sourcesBasePath;
+	/** @var bool Register PhpConsole\Helper that allows short debug calls like PC::debug($var, 'ta.g.s') */
+	protected $registerHelper = true;
+
+	/** @var string|null Server internal encoding */
+	protected $serverEncoding;
+	/** @var int|null Set headers size limit for your web-server. You can detect headers size limit by /PhpConsole/examples/utils/detect_headers_limit.php */
+	protected $headersLimit;
+	/** @var string|null Protect PHP Console connection by password */
+	protected $password;
+	/** @var bool Force connection by SSL for clients with PHP Console installed */
+	protected $enableSslOnlyMode = false;
+	/** @var array Set IP masks of clients that will be allowed to connect to PHP Console lie: array('192.168.*.*', '10.2.12*.*', '127.0.0.1') */
+	protected $ipMasks = array();
+
+	/** @var bool Enable errors handling */
+	protected $handleErrors = true;
+	/** @var bool Enable exceptions handling */
+	protected $handleExceptions = true;
+
+	/** @var int Maximum dumped vars array or object nested dump level */
+	protected $dumperLevelLimit = 5;
+	/** @var int Maximum dumped var same level array items or object properties number */
+	protected $dumperItemsCountLimit = 100;
+	/** @var int Maximum length of any string or dumped array item */
+	protected $dumperItemSizeLimit = 50000;
+	/** @var int Maximum approximate size of dumped vars result formatted in JSON */
+	protected $dumperDumpSizeLimit = 500000;
+	/** @var bool Convert callback items in dumper vars to (callback SomeClass::someMethod) strings */
+	protected $dumperDetectCallbacks = true;
+	/** @var bool Autodetect and append trace data to debug */
+	protected $detectDumpTraceAndSource = false;
+	/** @var \PhpConsole\Storage Postponed response storage */
+	protected $dataStorage = false;
+
+	/**
+	 * @var bool Enable eval request to be handled by eval dispatcher. Must be called after all Connector configurations.
+	 * $this->password is required to be set
+	 * use $this->ipMasks & $this->enableSslOnlyMode for additional protection
+	 */
+	public $isEvalEnabled = false;
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register() {
+	}
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		$this->package('php-console/php-console-laravel', static::PACKAGE_ALIAS);
+		foreach($this->getServiceProviderConfig() as $option => $value) {
+			$this->setOption($option, $value);
+		}
+		$this->initPhpConsole();
+	}
+
+	protected function getServiceProviderConfig() {
+		return Config::get(static::PACKAGE_ALIAS . '::config');
+	}
+
+	public function setOption($name, $value) {
+		if(!property_exists($this, $name)) {
+			throw new \Exception('Unknown property "' . $name . '" in ' . static::PACKAGE_ALIAS . ' package config');
+		}
+		$this->$name = $value;
+	}
+
+	protected function initPhpConsole() {
+		Connector::setPostponeStorage($this->dataStorage);
+
+		$connector = Connector::getInstance();
+
+		if($this->registerHelper) {
+			Helper::register();
+		}
+
+		if(!$this->isEnabled || !Connector::getInstance()->isActiveClient()) {
+			return;
+		}
+
+		$handler = Handler::getInstance();
+		$handler->setHandleErrors($this->handleErrors);
+		$handler->setHandleErrors($this->handleExceptions);
+		$handler->start();
+
+		if($this->sourcesBasePath) {
+			$connector->setSourcesBasePath($this->sourcesBasePath);
+		}
+		if($this->serverEncoding) {
+			$connector->setServerEncoding($this->serverEncoding);
+		}
+		if($this->password) {
+			$connector->setPassword($this->password);
+		}
+		if($this->enableSslOnlyMode) {
+			$connector->enableSslOnlyMode();
+		}
+		if($this->ipMasks) {
+			$connector->setAllowedIpMasks($this->ipMasks);
+		}
+		if($this->headersLimit) {
+			$connector->setHeadersLimit($this->headersLimit);
+		}
+
+		if($this->detectDumpTraceAndSource) {
+			$connector->getDebugDispatcher()->detectTraceAndSource = true;
+		}
+
+		$dumper = $connector->getDumper();
+		$dumper->levelLimit = $this->dumperLevelLimit;
+		$dumper->itemsCountLimit = $this->dumperItemsCountLimit;
+		$dumper->itemSizeLimit = $this->dumperItemSizeLimit;
+		$dumper->dumpSizeLimit = $this->dumperDumpSizeLimit;
+		$dumper->detectCallbacks = $this->dumperDetectCallbacks;
+
+		if($this->isEvalEnabled) {
+			$connector->startEvalRequestsListener();
+		}
+	}
+}
